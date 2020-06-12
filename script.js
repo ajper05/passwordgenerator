8c68309330616ef417a34aa8c3a55bc0b60d0ebe
Anthony <pokepics@hotmail.com>
2020-06-12 15:18:54 -0700

    user preferences work, just trying to get the password to write to the form now

--- script.js
+++ script.js
@@ -6,36 +6,64 @@ var upperList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var lowerList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialList = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+var userLower = confirm("Would you like lower case letters? Okay = Yes  Cancel = No");
+var userUpper = confirm("Would you like upper case letters? Okay = Yes  Cancel = No");
+var userNumeric = confirm("Would you like to have numbers? Okay = Yes  Cancel = No");
+var userSpecial = confirm("Would you like special characters? Okay = Yes  Cancel = No");
 
 
 //Grabs user preferred length
 function passwordLength(){
   userLength = prompt("How many characters? Min. 8 - Max. 128");
   if(userLength < 8 || userLength > 126 || userLength === NaN){
-    alert("Password must be a minimum of 8 characters and a maximum of 128");
+    alert("Password length must be a minimum of 8 characters and a maximum of 128");
     passwordLength();
   }
-  return userLength;
+  return parseInt(userLength);
   
 }
 
 //Gathers all other user preferences that can be stored as boolean type. 
 function checkPreferences(){
-  userLower = confirm("Would you like lower case letters? Okay = Yes  Cancel = No");
-  userUpper = confirm("Would you like upper case letters? Okay = Yes  Cancel = No");
-  userNumeric = confirm("Would you like to have numbers? Okay = Yes  Cancel = No");
-  userSpecial = confirm("Would you like special characters? Okay = Yes  Cancel = No")
+  var passwordResult = []
+  // userLower = confirm("Would you like lower case letters? Okay = Yes  Cancel = No");
+  // userUpper = confirm("Would you like upper case letters? Okay = Yes  Cancel = No");
+  // userNumeric = confirm("Would you like to have numbers? Okay = Yes  Cancel = No");
+  // userSpecial = confirm("Would you like special characters? Okay = Yes  Cancel = No");
 
-  console.log(userLower);
-  console.log(userUpper);
-  console.log(userNumeric);
-  console.log(userSpecial);
+  if (userLower = true){
+    passwordResult = passwordResult.concat(lowerList)
+  }
 
-
-  return userLower, userUpper, userNumeric, userSpecial;
+  if (userUpper = true){
+    passwordResult = passwordResult.concat(upperList)
+  }
   
+  if (userNumeric = true){
+    passwordResult = passwordResult.concat(numberList)
+  }
+
+  if (userSpecial = true){
+    passwordResult = passwordResult.concat(specialList)
+  }
+
+  return passwordResult;
+}
+//Call User Input Functions
+passwordLength(); 
+checkPreferences();
+
+//Select random characters from the passwordResult array using the defined user length and return the result. 
+function generatePassword(){
+  randomizedPassword = "";
+  for (i = 0; i < userLength; i++){
+    randomizedPassword += passwordResult[(Math.floor(Math.random() * userlength))]
+
+  return randomizedPassword;
+  }
 }
 
+generatePassword();
 
 // Write password to the #password input
 function writePassword() {
